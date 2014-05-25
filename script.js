242da50da5d8653025e6dcaa4bf06dd7a31f7ee7
Sören Gade <gade.soeren@googlemail.com>
2014-05-26 00:55:34 +0200

    Made it into an Angular.js Application.

--- script.js
+++ script.js
@@ -1,67 +1,28 @@
-$(function() {
+(function() {
   function runAsync(fn) {
     setTimeout(fn, 0);
   }
   
-  function setProgressBarByScore(score) {
-    var klass = 'zero';
-    switch ( score ) {
-      case 1:
-        klass = 'one';
-        break;
-      case 2:
-        klass = 'two';
-        break;
-      case 3:
-        klass = 'three';
-        break;
-      case 4:
-        klass = 'four';
-        break;
-    }
-    
-    $(".pw-widget .bar").attr('class', '').addClass('bar ' + klass);
-  }
-  function showEstimateStats(estimate) {
-    estimate = estimate || {};
-    var crackTimeDisplay = estimate.crack_time_display || 'unknown';
-    
-    $("#calc-time").text(estimate.calc_time);
-    $("#score").text(estimate.score);
-    $("#crack-time-display").text(crackTimeDisplay);
-    $("#crack-time").text(estimate.crack_time);
-    $("#entropy").text(estimate.entropy);
-  }
+  var passwordTool = angular.module("passwordTool", []);
   
-  var lastCheckedPw = null;
-  function runCheck(pw) {
-    if ( pw == lastCheckedPw ) {
-      return;
-    } else {
-      lastCheckedPw = pw;
-    }
-    runAsync(function() {
-      showEstimateStats(estimate);
+  passwordTool.controller("passwordStrengthController", function($scope) {
+    // startup values:
+    $scope.estimate = {
+      calc_time: "0",
+      score: 0,
+      crack_time: 0,
+      crack_time_display: "unknown",
+      entropy: 0
+    };
+    
+    $scope.password = "";
+    $scope.onPasswordChange = function() {
+      runAsync(function() {
+        console.log("async");
+        $scope.estimate = zxcvbn($scope.password);
       
-      if ( pw != $("#pw").val() ) {
-        return;
-      }
-      
-      var estimate = zxcvbn(pw);
-      
-      if ( pw == $("#pw").val() ) {
-        setProgressBarByScore(estimate.score);
-        showEstimateStats(estimate);
-      } else {
-        console.log("calculated '" + pw + "' but now its '" + $("#pw").val() + "'...");
-      }
-    });
-  }
-  
-  $("#pw").keyup(function() {
-    var pw = $("#pw").val();
-    runCheck(pw);
+        $scope.barSizeClass = "score-" + $scope.estimate.score;
+      });
+    };
   });
-  
-  runCheck($("#pw").val());
-});
+}());
